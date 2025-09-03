8d13d7008972de9b7c8afd9e67a916e0b1dc39ce
Your Actual Name <your-email@example.com>
2025-09-03 01:50:05 -0700

    Add local database environment variables

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('codecrowds', 'root', '', {
+const sequelize = new Sequelize('codecrowds', 'root', 'YOUR_MYSQL_PASSWORD', {
     host: 'localhost',
     dialect: 'mysql',
     logging: false
