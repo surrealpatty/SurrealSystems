76188a07ada55423c99237b6a2501c8e66fd9a09
Your Actual Name <your-email@example.com>
2025-09-03 11:11:20 -0700

    Add local database environment variables

---
+++ models/database.js
@@ -0,0 +1,10 @@
+// models/database.js
+const { Sequelize } = require('sequelize');
+const path = require('path');
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: path.join(__dirname, 'database.sqlite') // SQLite file
+});
+
+module.exports = sequelize;
